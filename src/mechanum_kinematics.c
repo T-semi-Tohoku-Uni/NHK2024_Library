965770d8e8393751d99f9376b740bf34c0ccac35
yuppy1005 <Y.Y.cricket@outlook.jp>
2024-03-08 20:51:17 +0900

    とりあえずFKとIKを書いた

---
+++ src/mechanum_kinematics.c
@@ -0,0 +1,56 @@
+//
+// Created by YYcri on 2024/03/08.
+//
+
+#include "mechanum_kinematics.h"
+//y+: forward, x+: right, omega: counter clockwise
+
+// mechanum wheel robot inverseKinematics
+// input: robotParams and velocity of robot {vx, vy, omega}
+// output: wheel angle velocity {w1, w2, w3, w4}
+void inverseKinematics(struct robotParams *params, double *velocity, double *wheelAngleVelocity) {
+    double vx = velocity[0];
+    double vy = velocity[1];
+    double omega = velocity[2];
+    double wheelDiameter = params->wheelDiameter;
+    double treadLen = params->treadLen;
+    double wheelBaseLen = params->wheelBaseLen;
+    double wheelParam = (treadLen + wheelBaseLen) / 2;
+
+    //convert matrix
+    double wheelMatrix[4][3] = {
+        {-1,  1, wheelParam},
+        {-1, -1, wheelParam},
+        { 1, -1, wheelParam},
+        { 1,  1, wheelParam}
+    };
+
+    //calculate wheel angle velocity
+    for (int i = 0; i < 4; i++) {
+        wheelAngleVelocity[i] = wheelMatrix[i][0] * vx + wheelMatrix[i][1] * vy + wheelMatrix[i][2] * omega;
+        wheelAngleVelocity[i] = wheelAngleVelocity[i] / wheelDiameter;
+    }
+}
+
+// mechanum wheel robot forwardKinematics
+// input: robotParams and wheel angle velocity {w1, w2, w3, w4}
+// output: velocity of robot {vx, vy, omega}
+void forwardKinematics(struct robotParams *params, double *wheelAngleVelocity, double *velocity) {
+    double wheelDiameter = params->wheelDiameter;
+    double treadLen = params->treadLen;
+    double wheelBaseLen = params->wheelBaseLen;
+    double wheelParam = (treadLen + wheelBaseLen) / 2;
+
+    //convert matrix
+    const float wheelMatrix[3][4] = {
+    {-1, -1, 1, 1},
+    {1, -1, -1, 1},
+    {1/wheelParam, 1/wheelParam, 1/wheelParam, 1/wheelParam}
+    };
+
+    //calculate velocity
+    for (int i = 0; i < 3; i++) {
+        velocity[i] = wheelMatrix[i][0] * wheelAngleVelocity[0] + wheelMatrix[i][1] * wheelAngleVelocity[1] + wheelMatrix[i][2] * wheelAngleVelocity[2] + wheelMatrix[i][3] * wheelAngleVelocity[3];
+        velocity[i] = velocity[i] * wheelDiameter / 4;
+    }
+}
---
+++ src/mechanum_kinematics.h
@@ -0,0 +1,14 @@
+//
+// Created by Yoshi on 2024/03/08.
+//
+
+#ifndef NHK2024_LIBRARY_MECHANUM_KINEMATICS_H
+#define NHK2024_LIBRARY_MECHANUM_KINEMATICS_H
+//robot wheel parameters with struct
+struct robotParams {
+    double wheelDiameter;
+    double treadLen;
+    double wheelBaseLen;
+};
+
+#endif //NHK2024_LIBRARY_MECHANUM_KINEMATICS_H
